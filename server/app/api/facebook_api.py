339ac20e3f6292d753c94d78659ebfdd33adac7d
Adnan Makhani <adnan.makhani@quartett-mobile.de>
2017-11-18 00:49:32 +0100

    get all the images with x and y

--- server/app/api/facebook_api.py
+++ server/app/api/facebook_api.py
@@ -2,6 +2,8 @@ import facebook as fb
 from flask import request, jsonify
 from flask_restful import Resource
 
+FB_API_TOKEN = "EAACEdEose0cBANR43pM6zG8YekjRpPsVFOdT7tdR9ij7LDF9w4RJ3x6Uk3xkRgb2RogUbA4kqiLb4BFKkpuD3z9oHgmc2HdKeVHGaqTDMYy8sIPMkvtIpYqX4xfmiO5G8QH90kxxoR1Xrgu9UosZAEmL0giHjiUp68h4S7U78QZAVZCqRV3hYc4cDAkj5x39ZAMTn7c9YgZDZD"
+fb_graph = fb.GraphAPI(access_token=FB_API_TOKEN)
 
 class FacebookAPI(Resource):
 
@@ -12,9 +14,6 @@ class FacebookAPI(Resource):
             return response
         
         id = id + "/friends"
-
-        FB_API_TOKEN = "EAACEdEose0cBANR43pM6zG8YekjRpPsVFOdT7tdR9ij7LDF9w4RJ3x6Uk3xkRgb2RogUbA4kqiLb4BFKkpuD3z9oHgmc2HdKeVHGaqTDMYy8sIPMkvtIpYqX4xfmiO5G8QH90kxxoR1Xrgu9UosZAEmL0giHjiUp68h4S7U78QZAVZCqRV3hYc4cDAkj5x39ZAMTn7c9YgZDZD"
-        fb_graph = fb.GraphAPI(access_token=FB_API_TOKEN)
         info = fb_graph.get_object(id=id, fields="name,photos")
 
         output = []
@@ -23,8 +22,23 @@ class FacebookAPI(Resource):
             if "photos" in user:
                 for photo in user["photos"]["data"]:
                     photo_id = photo["id"]
-                    output.append(user["name"] + " " + photo_id)
+                    image, x, y = FacebookAPI.tags(user["name"],photo_id)
+                    output.append(user["name"] + " " + image + "; x: " + str(x) + "; y: " + str(y))
 
         response = jsonify(output)
         response.status_code = 200
         return response
+    
+    def tags(name, photo_id):
+        tagged_photos = fb_graph.get_object(id=photo_id, fields="images,tags")
+        image = tagged_photos["images"][0]["source"]
+
+        for tags in tagged_photos["tags"]["data"]:
+            if tags["name"] == name:
+                return image,tags["x"], tags["y"]
+            
+        return image, 0, 0
+
+            
+
+        
