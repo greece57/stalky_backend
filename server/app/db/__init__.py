0dbb0f944a1e8e4f2320ac68eced960a3c3762bb
Niko <greece93@gmail.com>
2017-11-18 22:37:14 +0100

    we need nicer names!

--- server/app/db/__init__.py
+++ server/app/db/__init__.py
@@ -2,3 +2,6 @@
 import peewee as pw
 
 DB = pw.MySQLDatabase("stalky", host="db", port=3306, user="stalky", passwd="stalky", charset='utf8mb4', use_speedups=False)
+
+# to be able to store emojis in the database:
+DB.execute_sql("ALTER DATABASE stalky DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci")
