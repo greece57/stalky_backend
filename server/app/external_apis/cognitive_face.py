fe0b03c13bff090b25e35a4893d990bf42af56b2
Adnan Makhani <adnan.makhani@quartett-mobile.de>
2017-11-18 23:26:02 +0100

    dumb app smart backend

--- server/app/external_apis/cognitive_face.py
+++ server/app/external_apis/cognitive_face.py
@@ -33,7 +33,7 @@ class FaceApi():
 
         CF.person_group.train(person_group_id)
 
-    def identify_face(self, id, image_url):
+    def identify_face(self, id, image_url, x, y, width, height):
         print("alive 1")
         faces = CF.face.detect(image_url)
         print(len(faces))
@@ -41,13 +41,32 @@ class FaceApi():
             print("alive 2")
             identify_response = CF.face.identify([faces[0]["faceId"]], id)
             print(identify_response)
-            canidate = identify_response[0]["candidates"][0]
+            if len(identify_response[0]["candidates"]):
+                canidate = identify_response[0]["candidates"][0]
+            else:
+                return 1,0
             print(canidate)
             print("alive 4")
             if Friend.select().where(Friend.person_id == canidate["personId"]).exists():
                 return Friend().get(Friend.person_id == canidate["personId"]), canidate["confidence"]
             else:
                 return 0, 0
+        elif len(faces) > 1:
+            for face in faces:
+                rect = face["faceRectangle"]
+                if(overlap(x,x+width,y, y+height,rect["left"],rect["left"]+width,rect["top"],rect["top"]+height)):
+                    identify_response = CF.face.identify([face["faceId"]], id)
+                    if len(identify_response[0]["candidates"]):
+                        canidate = identify_response[0]["candidates"][0]
+                    else:
+                        return 1,0
+                    if Friend.select().where(Friend.person_id == canidate["personId"]).exists():
+                        return Friend().get(Friend.person_id == canidate["personId"]), canidate["confidence"]
+                    else:
+                        return 0, 0
+                else:
+                    return 0,0
+
         return 0,0
 
 
@@ -63,3 +82,6 @@ class FaceApi():
         return (rect_x <= fb_x <= rect_x + rect_width and
                 rect_y <= fb_y <= rect_y + rect_height)
 
+    def overlap(recta_left, recta_right, recta_top, recta_bottom, rectb_left, rectb_right, rectb_top, rectb_bottom):
+        return (recta_left < rectb_right && recta_right > rectb_left &&
+     recta_top > rectb_bottom && recta_bottom < rectb_top ) 
