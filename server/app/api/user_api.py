7895b08306772da593b8cbe3fa39f6fb7f0c849a
Adnan Makhani <adnan.makhani@quartett-mobile.de>
2017-11-18 03:52:10 +0100

    checking the right face

--- server/app/api/user_api.py
+++ server/app/api/user_api.py
@@ -1,15 +1,32 @@
 import threading
 from flask import request, jsonify
 from flask_restful import Resource
+from app.model import Image
 
 from app.helper.func_thread import FuncThread
 from app.external_apis.fb_graph import FbGraph
 
+import cognitive_face as CF
 
 class UserApi(Resource):
 
     def get(self):
-        response = jsonify({"OK":"works"})
+        image = Image.get()
+        url = image.source_url
+        face_exist = []
+        KEY = 'e6caf64b32b24455a6a4e2e91a386f76'  
+        CF.Key.set(KEY)
+        BASE_URL = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/'  
+        CF.BaseUrl.set(BASE_URL)
+
+        result = CF.face.detect(url)
+
+        for face in result:
+            rect = face["faceRectangle"]
+            exists = UserApi.contains(image.x, image.y, rect["left"], rect["top"],rect["width"], rect["height"])
+            face_exist.append(exists)
+
+        response = jsonify(face_exist)
         response.status_code = 200
         return response
 
@@ -25,6 +42,11 @@ class UserApi(Resource):
         response = jsonify({"OK":"Adding User"})
         response.status_code = 200
         return response
+
+    def contains(fb_x, fb_y, rect_x, rect_y, rect_width, rect_height):
+        return (rect_x <= fb_x <= rect_x + rect_width and
+            rect_y <= fb_y <= rect_y + rect_height)
+
     
 
             
